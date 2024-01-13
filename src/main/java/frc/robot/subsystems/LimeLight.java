4f3dcdf307f055a2aa47ef5a1b02e5e61c584ff1
SomnolentStone <jacksonstone0709@gmail.com>
2024-01-13 13:28:18 -0700

    Added Limelight methods

--- src/main/java/frc/robot/subsystems/LimeLight.java
+++ src/main/java/frc/robot/subsystems/LimeLight.java
@@ -3,6 +3,8 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
+//Java Imports
+import java.util.Timer; //Unused, but here.
 
 public class LimeLight {
     //setup networktable upon creation
@@ -15,6 +17,7 @@ public class LimeLight {
     private double currentY; // Y value is vertical angle from center of LL camera
     private double currentArea; // Unknown what this does currently.
 
+    //CONSTANTS
     //Physical distance of limelight LENS from ground (measured in INCHES)
     private final double LensDistFromGround = 0.0;
     //Physical vertical angle of lens from mount (measured in DEGREES).
@@ -22,6 +25,11 @@ public class LimeLight {
     //Physical height of chosen AprilTag.
     //If needed, create a table that holds the AprilTag IDs and its height from the ground.
     private final double targetHeight = 0.0;
+    //Correction modifier. I assume it designates how much of a correction you want.
+    private final double correctionMod = -.1;
+    //Preset distance from target.
+    //Could put it in an array and designate it to an AprilTag.
+    private final double desiredDist = 36.0;
 
     //# LIMELIGHT
     /* Constructor. Assigns values to the coordinate variables above.
@@ -36,28 +44,36 @@ public class LimeLight {
         SmartDashboard.putNumber("LimelightY", currentY);
         SmartDashboard.putNumber("LimelightArea", currentArea);
     }
-
-    public double getDistFromTarget(){
+    //#ESTIMATEDIST
+    /* Does math to estimate the distance from the limelight to the target.
+        Assumes that robot is centered on target.
+     */
+    public double estimateDist(){
         double radAngle = Math.toRadians(currentY + LensAngleFromMount);
 
-        //Simple trigonometry to return distance from given angle (inverse tan function)
+        //Simple trigonometry to return distance from given angle 
         double distFromGoal = (targetHeight - LensDistFromGround) / Math.tan(radAngle);
         return distFromGoal;
     }
-    public static void main(String[] args){
-        NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
-        NetworkTableEntry tx = table.getEntry("tx");
-        NetworkTableEntry ty = table.getEntry("ty");
-        NetworkTableEntry ta = table.getEntry("ta");
+    //#GETINRANGE
+    /* Auto-Function that allows the robot to get in range of a given target (currently target ID 1).
+     * Not fully optimized currently, slows as you get closer.
+     */
+    public boolean getInRange(DriveTrain driveTrain){
+        double currentDist = estimateDist();
+        double distError = desiredDist - currentDist; //Distance from desired point.
 
-        //read values periodically
-        double x = tx.getDouble(0.0);
-        double y = ty.getDouble(0.0);
-        double area = ta.getDouble(0.0);
+        while (distError > .5 || distError < -.5){
+            double drivingAdjust  = (correctionMod * distError) * .8; //Basically designates speed.
+            if (drivingAdjust < -.5)
+                drivingAdjust = -.5;
+            else if (drivingAdjust < .5)
+                drivingAdjust = .5;
+            double turnPower = currentX / 10.0;
+            driveTrain.HamsterDrive.arcadeDrive(drivingAdjust, turnPower);
+        }
 
-        //post to smart dashboard periodically
-        SmartDashboard.putNumber("LimelightX", x);
-        SmartDashboard.putNumber("LimelightY", y);
-        SmartDashboard.putNumber("LimelightArea", area);
+        return true;
     }
+
 }
